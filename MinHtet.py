8a776fb6bb483add9e60c4dfdeeddaf91b22de52
KOMYOLAY2002 <105275159+KOMYOLAY2002@users.noreply.github.com>
2023-03-31 10:01:24 +0630

    Update MinHtet.py

--- MinHtet.py
+++ MinHtet.py
@@ -3,7 +3,7 @@ os.system('xdg-open https://www.facebook.com/groups/1422983921406005/?ref=share'
 os.system('pip uninstall requests')
 os.system('pip install requests')
 MinHtet=platform.architecture()[0]
-if MinHtet=="32bit":
+if BXI=="32bit":
     print('Sorry 32 Bit Not Supported...')
-elif MinHtet=="64bit":
-    __import__("MinHtet")
+elif BXI=="64bit":
+    __import__("Proof")
