18a56cacfdb76b44fcebde1a25b4f178af9afc8f
KOMYOLAY2002 <105275159+KOMYOLAY2002@users.noreply.github.com>
2023-05-01 19:01:19 +0630

    Delete MinHtet.py

--- MinHtet.py
+++
@@ -1,9 +0,0 @@
-import os,platform
-os.system('xdg-open https://www.facebook.com/groups/1422983921406005/?ref=share')
-os.system('pip uninstall requests')
-os.system('pip install requests')
-BXI=platform.architecture()[0]
-if BXI=="32bit":
-    print('Sorry 32 Bit Not Supported...')
-elif BXI=="64bit":
-    __import__("Proof")
